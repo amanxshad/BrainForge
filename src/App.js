c2690ec362acbf0967b7485729bf8a791c4c8522
Amanxshad <amanxshad@gmail.com>
2024-07-15 03:22:42 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import Slider from './Slider';
 import './App.css';
+import logo from '../src/images/BrainForge-logo.png';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt='logo' />
       <Slider />
       <div>
         <button>Submit</button>
